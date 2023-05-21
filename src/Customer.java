ae939f0b8c5cc3effd3920c428d6afe7c5b484eb
yourisev <sevgama44@gmail.com>
2023-05-21 18:07:35 -0400

    Added an Association(has-a) relationship between BankAccount and Customer, since Customer contains BankAccount but BankAccount van exist without Customer this is a directional association called Aggregation.

---
+++ src/Customer.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Customer {
+    private int id;
+    private String name;
+    private List<BankAccount> accounts;
+
+    public void createAccount(){
+
+    }
+}
